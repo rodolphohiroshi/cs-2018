95d967a681c6900ff48f65a652173dce7e6eb2d4
Rodolpho Hiroshi <rodolphohiroshi@gmail.com>
2018-05-09 01:10:56 -0300

    Commit propriedade153

---
+++ topico-4/Propriedade153.js
@@ -0,0 +1,14 @@
+let num = 153;
+
+if(num<0||num>9999){
+    process.exitCode = -1;
+}
+let c = ~~(num/100);
+let du = num%100;
+let d = ~~(du/10);
+let u = du%10;
+
+let result = (Math.pow((c),3)+Math.pow((d),3)+Math.pow((u),3));
+if(num == result){
+    console.log("verdadeiro");
+}else console.log("falso");
