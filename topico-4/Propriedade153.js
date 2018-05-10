c185fba116ceb3021e88ab7b1b37f71310fadb6b
Rodolpho Hiroshi <rodolphohiroshi@gmail.com>
2018-05-09 21:12:38 -0300

    Melhoria Propriedade 153

--- topico-4/Propriedade153.js
+++ topico-4/Propriedade153.js
@@ -1,14 +1,14 @@
-let num = 153;
+function propriedade153(n){
 
-if(num<0||num>9999){
-    process.exitCode = -1;
+    if( n < 0 || n > 9999 ){
+        return false;
+    }
+    let c = Math.floor(n / 100);
+    let du = n % 100;
+    let d = Math.floor(du / 10);
+    let u = du % 10;
+
+    let result = (Math.pow((c),3)+Math.pow((d),3)+Math.pow((u),3));
+    return result;
 }
-let c = ~~(num/100);
-let du = num%100;
-let d = ~~(du/10);
-let u = du%10;
-
-let result = (Math.pow((c),3)+Math.pow((d),3)+Math.pow((u),3));
-if(num == result){
-    console.log("verdadeiro");
-} else process.exitCode = -1;
+exports.propriedade153 = propriedade153;
